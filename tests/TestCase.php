2bf9981f8d2556158d2e729f76758fd7e32d7316
ivan-isaev21 <isaev.ivan.aleksandrovich@gmail.com>
2023-07-08 17:04:40 +0300

    Add tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,21 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, RefreshDatabase;
+
+    public function refreshDatabase()
+    {
+        $this->usingInMemoryDatabase()
+            ? $this->refreshInMemoryDatabase()
+            : $this->refreshTestDatabase();
+
+        // $this->seed();
+        // $this->artisan('passport:install');
+    }
+   
 }
---
+++ tests/Unit/RegisterTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use Tests\TestCase;
+
+class RegisterTest extends TestCase
+{
+
+    /**
+     * Method test_register
+     *
+     * @return void
+     */
+    public function test_register(): void
+    {
+        $user = User::register(
+            $name = 'name',
+            $email = 'email',
+            $password = 'password'
+        );
+
+        $this->assertNotEmpty($user);
+        $this->assertEquals($name, $user->name);
+        $this->assertEquals($email, $user->email);
+        $this->assertNotEmpty($user->password);
+        $this->assertNotEquals($password, $user->password);
+
+        $this->assertTrue($user->isWait());
+        $this->assertFalse($user->isActive());
+        $this->assertFalse($user->isAdmin());
+    }
+
+    /**
+     * Method test_verify
+     *
+     * @return void
+     */
+    public function test_verify()
+    {
+        $user = User::register('name', 'email', 'password');
+        $user->verify();
+        $this->assertFalse($user->isWait());
+        $this->assertTrue($user->isActive());
+    }
+    
+    /**
+     * Method test_already_verified
+     *
+     * @return void
+     */
+    public function test_already_verified()
+    {
+        $user = User::register('name', 'email', 'password');
+        $user->verify();
+        $this->expectExceptionMessage('User is already verified.');
+        $user->verify();
+    }
+}
