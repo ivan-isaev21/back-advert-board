7347cdf0174b9f7d85b082501489d0073fadbd15
ivan-isaev21 <isaev.ivan.aleksandrovich@gmail.com>
2023-07-29 17:44:33 +0300

    Change foreign

--- database/migrations/2023_07_16_125808_create_advert_property_boolean_values_table.php
+++ database/migrations/2023_07_16_125808_create_advert_property_boolean_values_table.php
@@ -12,11 +12,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('advert_property_boolean_values', function (Blueprint $table) {
-            $table->unsignedBigInteger('advert_id')->references('id')->on('adverts')->cascadeOnDelete()->cascadeOnUpdate();
-            $table->unsignedBigInteger('property_id')->references('id')->on('advert_properties')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->id();
+            $table->unsignedBigInteger('advert_id');
+            $table->unsignedBigInteger('property_id');
             $table->boolean('value')->default(false);
             $table->timestamps();
-            $table->primary(['advert_id', 'property_id']);
+            $table->unique(['advert_id', 'property_id']);
+
+            $table->foreign('advert_id')->references('id')->on('adverts')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreign('property_id')->references('id')->on('advert_properties')->cascadeOnDelete()->cascadeOnUpdate();
         });
     }
 
